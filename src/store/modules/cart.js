a06d23faf91c505075daa9fe6d80b66cbfd6734a
Cassie-Ch-0909 <158829893+Cassie-Ch-0909@users.noreply.github.com>
2024-02-08 15:57:04 +0800

    在获取购物车列表方法中给购物车的每一项添加isChecked属性，以更好实现购物车管理功能

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -41,8 +41,14 @@ export default {
     */
     async getCartListAction(context, userId) {
       const res = await getCartListAPI(userId);
-      context.commit("setCartList", res.data);
       // console.log(context.state.cartList);
+      // 后台返回的数据中，不包含复选框的选中状态，为了实现将来的功能
+      // 需要手动维护数据，给每一项，添加一个isChecked状态 标记当前商品是否选中
+      // 默认为选中状态
+      res.data.forEach((item) => {
+        item.isChecked = true;
+      });
+      context.commit("setCartList", res.data);
     },
   },
   getters: {},
